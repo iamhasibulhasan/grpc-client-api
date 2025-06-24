ff1dc91d356abd33e290062f3000efb97b7f1f83
iamhasibulhasan <hashibul218@gmail.com>
2025-06-24 18:05:51 +0600

    Add project files.

---
+++ grpc-client-api/Program.cs
@@ -0,0 +1,39 @@
+using grpc_client_api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Register the gRPC client with the address from configuration
+builder.Services.AddSingleton<GrpcGreeterClient>(provider =>
+{
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    var serverAddress = configuration["GrpcServer"];
+
+    return new GrpcGreeterClient(configuration);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
